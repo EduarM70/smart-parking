9c2873a5d1a69a6a7bfa123b4b732efe7693dc1b
StevenRamirez25 <santos20054ramirezsteven@gmail.com>
2025-05-16 23:37:22 -0600

    Cambio de prueba

--- SmartParking/SmartParking/Program.cs
+++ SmartParking/SmartParking/Program.cs
@@ -17,7 +17,7 @@ namespace SmartParking
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new PruebaDibujoForm());
         }
     }
 }
